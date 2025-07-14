69999471c60a487a1aa8576476a1d757aab8e9b6
Hastak <ahastak@gmail.com>
2025-07-14 13:00:39 +0900

    get api

---
+++ lib/types.d.ts
@@ -0,0 +1,8 @@
+import type { components } from './api-schema';
+
+export type CreateProjectDTO = components['schemas']['CreateProjectDTO'];
+export type Project = components['schemas']['Project'];
+export type Task = components['schemas']['Task'];
+export type UpdateProjectDTO = components['schemas']['UpdateProjectDTO'];
+export type CreateTaskDTO = components['schemas']['CreateTaskDTO'];
+export type UpdateTaskDTO = components['schemas']['UpdateTaskDTO'];
