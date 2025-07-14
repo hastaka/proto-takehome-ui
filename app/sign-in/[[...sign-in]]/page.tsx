bad1cf3b027727cd495af93c567dec7e312af054
Hastak <ahastak@gmail.com>
2025-07-15 08:35:54 +0900

    remove stray semicolon

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -5,7 +5,7 @@ import { SignIn } from '@clerk/nextjs';
 export default function Page() {
     return (
         <main className="flex h-full items-center justify-center p-8 sm:p-20">
-            <SignIn />;
+            <SignIn />
         </main>
     );
 }
