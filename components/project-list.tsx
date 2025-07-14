1e9820e254ba1a13f4ef55af9bb8cbfb9ee355b6
Hastak <ahastak@gmail.com>
2025-07-14 16:33:45 +0900

    update loaders and sorting

--- components/project-list.tsx
+++ components/project-list.tsx
@@ -3,24 +3,56 @@ import useProjects from '@/hooks/use-projects';
 import { ScrollArea } from './ui/scroll-area';
 import { useSearchParams } from 'next/navigation';
 import ProjectListItem from './project-list-item';
+import { Alert, AlertDescription, AlertTitle } from './ui/alert';
+import { AlertOctagon, Loader } from 'lucide-react';
+import { Dialog, DialogTrigger } from './ui/dialog';
+import { Button } from './ui/button';
+import { EditProjectDialog } from './edit-project-dialog';
 
 export function ProjectList() {
     const { data, isLoading, error } = useProjects();
     const searchParams = useSearchParams();
     const projectId = searchParams.get('p');
 
-    if (isLoading || !data) return <div className='w-full h-full italic flex items-center justify-center'>Loading...</div>;
-    if (error) return <div className='w-full h-full italic flex items-center justify-center'>Error loading projects!</div>;
+    if (isLoading || !data)
+        return (
+            <div className="w-full h-full italic flex items-center justify-center">
+                <Alert className="w-auto">
+                    <Loader className="animate-spin" />
+                    <AlertTitle>Loading projects...</AlertTitle>
+                </Alert>
+            </div>
+        );
+    if (error)
+        return (
+            <div className="w-full h-full italic flex items-center justify-center">
+                <Alert variant="destructive" className="w-auto">
+                    <AlertOctagon />
+                    <AlertTitle>Error loading projects.</AlertTitle>
+                    <AlertDescription>
+                        {error.message + (error.message.endsWith('.') ? '' : '.') || 'Something went wrong.'}
+                        <br />
+                        Please try again later.
+                    </AlertDescription>
+                </Alert>
+            </div>
+        );
 
     return (
         <div className="flex-1 overflow-hidden">
             <ScrollArea className="h-full pr-2">
                 <div className="flex flex-col gap-2">
-                    {data.map((p, index) => {
-                        return (
-                            <ProjectListItem key={index} active={p.id === projectId} project={p} />
-                        );
+                    {data.sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime()).map((p, index) => {
+                        return <ProjectListItem key={index} active={p.id === projectId} project={p} />;
                     })}
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button className="w-full h-16 border-dashed italic" variant="outline">
+                                Create project
+                            </Button>
+                        </DialogTrigger>
+                        <EditProjectDialog create />
+                    </Dialog>
                 </div>
             </ScrollArea>
         </div>
--- components/task-list.tsx
+++ components/task-list.tsx
@@ -4,7 +4,7 @@ import { ScrollArea } from './ui/scroll-area';
 import useProjectTasks from '@/hooks/use-project-tasks';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
-import { MoreHorizontal } from 'lucide-react';
+import { AlertOctagon, Clock, Loader, Loader2, MoreHorizontal } from 'lucide-react';
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -22,6 +22,7 @@ import { Task } from '@/lib/types';
 import { Dialog, DialogTrigger } from './ui/dialog';
 import { EditTaskDialog } from './edit-task-dialog';
 import { useState } from 'react';
+import { Alert, AlertDescription, AlertTitle } from './ui/alert';
 
 const badgeMap: Record<string, 'destructive' | 'default' | 'secondary'> = {
     todo: 'destructive',
@@ -31,7 +32,7 @@ const badgeMap: Record<string, 'destructive' | 'default' | 'secondary'> = {
 
 const statusOrder = ['todo', 'in_progress', 'done'];
 
-function taskSort(t1:Task, t2:Task) {
+function taskSort(t1: Task, t2: Task) {
     const t1Index = statusOrder.indexOf(t1.status);
     const t2Index = statusOrder.indexOf(t2.status);
     if (t1Index < t2Index) return -1;
@@ -45,68 +46,103 @@ export function TaskList({ projectId }: { projectId: string }) {
     const deleteTask = useDeleteTask(projectId);
     const updateTask = useUpdateTask(projectId);
 
-    if (isLoading) return <div>Loading...</div>;
-    if (error) return <div>Error loading users</div>;
+    if (isLoading || !data)
+        return (
+            <div className="w-full h-full italic flex items-center justify-center">
+                <Alert className="w-auto">
+                    <Loader className="animate-spin" />
+                    <AlertTitle>Loading tasks...</AlertTitle>
+                </Alert>
+            </div>
+        );
+    if (error)
+        return (
+            <div className="w-full h-full italic flex items-center justify-center">
+                <Alert variant="destructive" className="w-auto">
+                    <AlertOctagon />
+                    <AlertTitle>Error loading tasks.</AlertTitle>
+                    <AlertDescription>
+                        {error.message + (error.message.endsWith('.') ? '' : '.') || 'Something went wrong.'}
+                        <br />
+                        Please select a project from the menu, or try again later.
+                    </AlertDescription>
+                </Alert>
+            </div>
+        );
 
     return (
         <div className="flex-1 overflow-hidden">
             <ScrollArea className="h-full pr-2">
                 <div className="flex flex-col gap-2">
-                    {data?.sort((t1, t2) => taskSort(t1, t2)).map((t) => (
-                        <div className="w-full p-2 rounded border flex flex-col gap-1" key={t.id}>
-                            <div className="flex gap-5 items-center">
-                                <h3 className="text-lg font-bold">{t.title}</h3>
-                                <Badge className={cn('text-sm font-bold', t.status === 'done' ? 'bg-green-700' : '')} variant={badgeMap[t.status]}>
-                                    {t.status.toUpperCase().replace('_', ' ')}
-                                </Badge>
-                                {t.due_date ? (
-                                    <p className="text-sm">{new Date(t.due_date).toLocaleDateString()}</p>
+                    {data
+                        .sort((t1, t2) => taskSort(t1, t2))
+                        .map((t) => (
+                            <div className="w-full p-2 rounded border flex flex-col gap-1" key={t.id}>
+                                <div className="flex gap-5 items-center">
+                                    <h3 className="text-lg font-bold">{t.title}</h3>
+                                    <Badge className={cn('text-sm font-bold', t.status === 'done' ? 'bg-green-700' : '')} variant={badgeMap[t.status]}>
+                                        {t.status.toUpperCase().replace('_', ' ')}
+                                    </Badge>
+                                    {t.due_date ? (
+                                        <p className="text-sm">{new Date(t.due_date).toLocaleDateString()}</p>
+                                    ) : (
+                                        <p className="text-sm text-muted-foreground italic">(no due date)</p>
+                                    )}
+                                    <Dialog>
+                                        <DropdownMenu>
+                                            <DropdownMenuTrigger asChild>
+                                                <Button variant="ghost" className="ml-auto" size="icon">
+                                                    <MoreHorizontal />
+                                                </Button>
+                                            </DropdownMenuTrigger>
+                                            <DropdownMenuContent side="left" align="start">
+                                                <DropdownMenuLabel className="text-muted-foreground text-xs">Status</DropdownMenuLabel>
+                                                <DropdownMenuRadioGroup
+                                                    value={t.status}
+                                                    onValueChange={(value) =>
+                                                        updateTask.mutate({ taskId: t.id, changes: { status: value as 'done' | 'todo' | 'in_progress' } })
+                                                    }
+                                                >
+                                                    <DropdownMenuRadioItem value="todo">Todo</DropdownMenuRadioItem>
+                                                    <DropdownMenuRadioItem value="in_progress">In Progress</DropdownMenuRadioItem>
+                                                    <DropdownMenuRadioItem value="done">Done</DropdownMenuRadioItem>
+                                                </DropdownMenuRadioGroup>
+                                                <DropdownMenuSeparator />
+                                                <DialogTrigger asChild>
+                                                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                                                </DialogTrigger>
+                                                <DropdownMenuItem
+                                                    variant="destructive"
+                                                    onClick={(e) => {
+                                                        e.preventDefault();
+                                                        deleteTask.mutate(t.id);
+                                                    }}
+                                                    disabled={deleteTask.isPending}
+                                                >
+                                                    {deleteTask.isPending ? 'Deleting...' : 'Delete'}
+                                                </DropdownMenuItem>
+                                            </DropdownMenuContent>
+                                        </DropdownMenu>
+                                        <EditTaskDialog taskId={t.id} projectId={projectId} />
+                                    </Dialog>
+                                </div>
+                                <p className="text-xs font-mono text-muted-foreground">{t.id}</p>
+                                <Separator />
+                                {t.description ? (
+                                    <p className="text-sm p-1">{t.description}</p>
                                 ) : (
-                                    <p className="text-sm text-muted-foreground italic">(no due date)</p>
+                                    <p className="text-sm text-muted-foreground italic">No description.</p>
                                 )}
-                                <Dialog>
-                                <DropdownMenu>
-                                    <DropdownMenuTrigger asChild>
-                                        <Button variant="ghost" className="ml-auto" size="icon">
-                                            <MoreHorizontal />
-                                        </Button>
-                                    </DropdownMenuTrigger>
-                                    <DropdownMenuContent side="left" align="start">
-                                        <DropdownMenuLabel className="text-muted-foreground text-xs">Status</DropdownMenuLabel>
-                                        <DropdownMenuRadioGroup value={t.status} onValueChange={(value) => updateTask.mutate({taskId: t.id, changes: { status: value as "done" | "todo" | "in_progress" }})}>
-                                            <DropdownMenuRadioItem value="todo">Todo</DropdownMenuRadioItem>
-                                            <DropdownMenuRadioItem value="in_progress">In Progress</DropdownMenuRadioItem>
-                                            <DropdownMenuRadioItem value="done">Done</DropdownMenuRadioItem>
-                                        </DropdownMenuRadioGroup>
-                                        <DropdownMenuSeparator />
-                                        <DialogTrigger asChild>
-                                        <DropdownMenuItem>Edit</DropdownMenuItem>
-                                        </DialogTrigger>
-                                        <DropdownMenuItem
-                                            variant="destructive"
-                                            onClick={(e) => {
-                                                e.preventDefault();
-                                                deleteTask.mutate(t.id);
-                                            }}
-                                            disabled={deleteTask.isPending}
-                                        >
-                                            {deleteTask.isPending ? 'Deleting...' : 'Delete'}
-                                        </DropdownMenuItem>
-                                    </DropdownMenuContent>
-                                </DropdownMenu>
-                                <EditTaskDialog taskId={t.id} projectId={projectId} />
-                                </Dialog>
                             </div>
-                            <p className="text-xs font-mono text-muted-foreground">{t.id}</p>
-                            <Separator />
-                            {t.description ? (
-                                <p className="text-sm p-1">{t.description}</p>
-                            ) : (
-                                <p className="text-sm text-muted-foreground italic">No description.</p>
-                            )}
-                        </div>
-                    ))}
-                    <Dialog><DialogTrigger asChild><Button className="w-full h-16" variant="outline">Create task</Button></DialogTrigger><EditTaskDialog create projectId={projectId} /></Dialog>
+                        ))}
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <Button className="w-full h-16 border-dashed italic" variant="outline">
+                                Create task
+                            </Button>
+                        </DialogTrigger>
+                        <EditTaskDialog create projectId={projectId} />
+                    </Dialog>
                 </div>
             </ScrollArea>
         </div>
